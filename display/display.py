7fc2a8741266bf81b8c19b5fc6ad1def62ded800
Cameron <camm73@yahoo.com>
2020-01-11 14:50:00 -0800

    Updated button size

--- display/display.py
+++ display/display.py
@@ -74,7 +74,7 @@ class Display():
                 buttonCol = 0
                 buttonRow += 1
             name = self.cocktailNames[i]
-            self.cocktailButtons[i] = tk.Button(self.window, text=name, width = 20, height =10, command= lambda name=name: self.makeCocktail(name))
+            self.cocktailButtons[i] = tk.Button(self.window, text=name, width = 30, height =20, command= lambda name=name: self.makeCocktail(name))
             self.cocktailButtons[i].grid(column=buttonCol, row=buttonRow)
             buttonCol += 1
             i = i+1
