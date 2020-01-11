a9b89a88dad94deeaca1bf95e1f4f04126cabc75
Cameron <camm73@yahoo.com>
2020-01-11 15:07:12 -0800

    Button change

--- display/display.py
+++ display/display.py
@@ -76,7 +76,7 @@ class Display():
                 buttonCol = 0
                 buttonRow += 1
             name = self.cocktailNames[i]
-            self.cocktailButtons[i] = tk.Button(self.window, text=name, font=cocktailButtonFont, width = 30, height =20, command= lambda name=name: self.makeCocktail(name))
+            self.cocktailButtons[i] = tk.Button(self.window, text=name, font=cocktailButtonFont, width = 20, height =15, command= lambda name=name: self.makeCocktail(name))
             self.cocktailButtons[i].grid(column=buttonCol, row=buttonRow)
             buttonCol += 1
             i = i+1
