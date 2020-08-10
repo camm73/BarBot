7532a7e159f64f13f37ed74abd97f68bf5b21bf3
Cameron Miller <camm73@yahoo.com>
2020-08-09 21:15:15 -0700

    Added new libraries; Added not available prompt

--- display/src/pages/HomePage.js
+++ display/src/pages/HomePage.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './HomePage.css';
 
 class HomePage extends React.Component{
-    constructor(props){
-        super(props);
-    }
 
     state = {
         cocktailList: [],
@@ -15,8 +12,9 @@ class HomePage extends React.Component{
             <div className='HomePage'>
                 <h1 className='HeaderText'>BarBot</h1>
                 {this.state.cocktailList.length === 0 && (
-                    <div>
+                    <div className='NotAvailableContainer'>
                         <h3 className='NotAvailable'>No cocktails are available right now.</h3>
+                        <h3 className='NotAvailable'>Please check back later.</h3>
                     </div>
                 )}
             </div>
